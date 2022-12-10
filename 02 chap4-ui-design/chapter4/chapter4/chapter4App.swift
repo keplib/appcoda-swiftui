263d1481fceea5ad88ebd34eba1fa0bad2dea654
keplib <keplib@gmail.com>
2022-12-10 18:53:13 +0100

    Chap4 starting

---
+++ 02 chap4-ui-design/chapter4/chapter4/chapter4App.swift
@@ -0,0 +1,17 @@
+//
+//  chapter4App.swift
+//  chapter4
+//
+//  Created by Balázs Képli on 10/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct chapter4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
