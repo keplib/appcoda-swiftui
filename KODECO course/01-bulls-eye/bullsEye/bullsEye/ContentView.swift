455fd6c3f9abb9a282a102a24297c1f0bb9560bb
keplib <keplib@gmail.com>
2022-12-13 18:24:43 +0100

    Kodeco course initial commit

---
+++ KODECO course/01-bulls-eye/bullsEye/bullsEye/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  bullsEye
+//
+//  Created by Balázs Képli on 13/12/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ KODECO course/01-bulls-eye/bullsEye/bullsEye/bullsEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  bullsEyeApp.swift
+//  bullsEye
+//
+//  Created by Balázs Képli on 13/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct bullsEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
