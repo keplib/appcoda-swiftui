ffd32498f6fe7b220cef19d1ea6211f14684449b
keplib <keplib@gmail.com>
2022-12-13 19:09:29 +0100

    Episodes 1 - 5

--- KODECO course/01-bulls-eye/bullsEye/bullsEye/ContentView.swift
+++ KODECO course/01-bulls-eye/bullsEye/bullsEye/ContentView.swift
@@ -9,13 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text(" 🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+                .kerning(2.0)
+                .bold()
+                .multilineTextAlignment(.center)
+                .lineSpacing(4.0)
+                .font(.footnote)
+            Text("89")
+            HStack {
+                Text("1")
+                Slider(value: .constant(50), in: 1.0...100.0)
+                Text("100")
+            }
+            .padding(.horizontal)
+            Button {
+                
+            } label: {
+                Text("Hit me!")
+            }
+
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        ContentView()
+            .previewInterfaceOrientation(.landscapeLeft)
     }
 }
