8f0a51e7fd59b5fcff0e82b723468c3d66178abb
keplib <keplib@gmail.com>
2022-12-17 13:45:41 +0100

    Kodeco: Part 1 COMPLETE, game MVP

--- KODECO course/01-bulls-eye/bullsEye/bullsEye/Models/Game.swift
+++ KODECO course/01-bulls-eye/bullsEye/bullsEye/Models/Game.swift
@@ -8,12 +8,13 @@
 import Foundation
 
 struct Game {
-    var target: Int = 42
+    var target: Int = Int.random(in: 1...100)
     var totalScore: Int = 0
     var round: Int = 1
     
-    
     func points(sliderValue: Int) -> Int {
-        return 999
+        let difference: Int = abs(sliderValue - self.target)
+        let awardedPoints: Int = 100 - difference
+        return awardedPoints
     }
 }
---
+++ KODECO course/01-bulls-eye/bullsEye/bullsEyeTests/bullsEyeTests.swift
@@ -0,0 +1,37 @@
+//
+//  bullsEyeTests.swift
+//  bullsEyeTests
+//
+//  Created by Balázs Képli on 17/12/2022.
+//
+
+import XCTest
+@testable import bullsEye
+
+class bullsEyeTests: XCTestCase {
+    
+    var game: Game!
+    
+    override func setUpWithError() throws {
+        game = Game()
+    }
+
+    override func tearDownWithError() throws {
+        game = nil
+    }
+    
+
+    func testScorePositive() {
+        var guess = game.target + 5
+        var score = game.points(sliderValue: guess)
+        XCTAssertEqual(score, 95)
+    }
+    
+    func testScoreNegative() {
+        var guess = game.target - 5
+        var score = game.points(sliderValue: guess)
+        XCTAssertEqual(score, 95)
+    }
+
+
+}
