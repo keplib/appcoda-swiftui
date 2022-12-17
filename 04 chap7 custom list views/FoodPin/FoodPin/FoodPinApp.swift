fe92805a73aa2a59856404b3c23717fd5452c7f3
keplib <keplib@gmail.com>
2022-12-17 18:01:23 +0100

    AppCoda Chap7 Complete

---
+++ 04 chap7 custom list views/FoodPin/FoodPin/FoodPinApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodPinApp.swift
+//  FoodPin
+//
+//  Created by Balázs Képli on 17/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct FoodPinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RestaurantListView()
+        }
+    }
+}
---
+++ 04 chap7 custom list views/FoodPin/FoodPin/RestaurantListView.swift
@@ -0,0 +1,52 @@
+//
+//  ContentView.swift
+//  FoodPin
+//
+//  Created by Balázs Képli on 17/12/2022.
+//
+
+import SwiftUI
+
+struct RestaurantListView: View {
+    
+    var restaurantNames = ["Cafe Deadend","Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional","Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
+    
+    
+    var restaurantImages = ["cafedeadend", "homei", "teakha","cafeloisl", "petiteoyster", "forkee", "posatelier", "bourkestreetbakery", "haigh", "palomino", "upstate", "traif", "graham", "waffleandwolf", "fiveleaves", "cafelore", "confessional", "barrafina", "donostia", "royaloak", "cask"]
+    
+    
+    var body: some View {
+        
+        List {
+            ForEach(restaurantNames.indices, id:\.self) { index in
+                HStack (alignment: .top, spacing: 20) {
+                    Image(restaurantImages[index])
+                        .resizable()
+                        .frame(width: 120, height: 120)
+                        .cornerRadius(20)
+                    VStack (alignment: .leading) {
+                        Text(restaurantNames[index])
+                            .font(.system(.title2, design: .rounded))
+                        Text("Type")
+                            .font(.system(.body, design: .rounded))
+                        Text("Location")
+                            .font(.system(.subheadline, design: .rounded))
+                            .foregroundColor(.gray)
+                    }
+                }
+            }
+            .listRowSeparator(.hidden)
+        }
+        .listStyle(.plain)
+        
+    }
+}
+
+struct RestaurantListView_Previews: PreviewProvider {
+    static var previews: some View {
+        RestaurantListView()
+        
+        RestaurantListView()
+            .preferredColorScheme(.dark)
+    }
+}
