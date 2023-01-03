32f28cb50ddb464e9f3ad8352bfb7748637eeae4
keplib <keplib@gmail.com>
2023-01-03 19:48:58 +0100

    AppCoda Chap13: add extension to UIColor class

--- 04 chap7 custom list views/FoodPin/FoodPin/FoodPinApp.swift
+++ 04 chap7 custom list views/FoodPin/FoodPin/FoodPinApp.swift
@@ -12,8 +12,8 @@ struct FoodPinApp: App {
     
     init() {
         let navBarAppearance = UINavigationBarAppearance()
-        navBarAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.systemRed, .font: UIFont(name: "ArialRoundedMTBold", size: 35)!]
-        navBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.systemRed, .font: UIFont(name: "ArialRoundedMTBold", size: 20)!]
+        navBarAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor(red: 218, green: 96, blue: 51), .font: UIFont(name:"ArialRoundedMTBold", size: 35)!]
+        navBarAppearance.titleTextAttributes = [.foregroundColor: UIColor(red: 218, green: 96, blue: 51), .font: UIFont(name: "ArialRoundedMTBold", size: 20)!]
         navBarAppearance.backgroundColor = .clear
         navBarAppearance.backgroundEffect = .none
         navBarAppearance.shadowColor = .clear
